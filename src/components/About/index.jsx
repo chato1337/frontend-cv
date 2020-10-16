19fa33ed52204603cb5ffdcbff61d451ec0e513c
chato1337 <chato1337@gmail.com>
2020-10-15 23:08:02 -0500

    implements about section and mockup others

---
+++ src/components/About/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.scss";
+
+export const About = () => {
+	return (
+		<div className="About content-item">
+			<h3>ABOUT ME</h3>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti,
+				nihil. Culpa, quis ducimus! Odio deserunt illo consectetur, omnis
+				aperiam nostrum obcaecati fugiat incidunt iusto veritatis velit nisi
+				voluptatibus beatae provident!
+			</p>
+		</div>
+	);
+};
---
+++ src/components/Education/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Education = () => {
+	return (
+		<div className="Education content-item">
+			<h3>EDUCATION</h3>
+			<p>date title</p>
+			<p>date title</p>
+			<p>date title</p>
+		</div>
+	);
+};
+
+export default Education;
