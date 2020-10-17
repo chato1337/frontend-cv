729b9d2d56607d123f1db76c133547b083898029
chato1337 <chato1337@gmail.com>
2020-10-17 16:20:32 -0500

    implements education section

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import "./education.scss";
 
 const Education = () => {
 	return (
 		<div className="Education content-item">
 			<h3>EDUCATION</h3>
-			<p>date title</p>
-			<p>date title</p>
-			<p>date title</p>
+			<div className="Education__content">
+				<div className="year">
+					<strong>2020</strong>
+				</div>
+				<div className="description">
+					<p>
+						systems engineering <small>(on curse)</small>
+					</p>
+
+					<strong>UNAD</strong>
+				</div>
+				<div className="year">
+					<strong>2017</strong>
+				</div>
+				<div className="description">
+					<p>computer architecture</p>
+
+					<strong>SENA</strong>
+				</div>
+				<div className="year">
+					<strong>2016</strong>
+				</div>
+				<div className="description">
+					<p>system technique</p>
+
+					<strong>ITGEM</strong>
+				</div>
+			</div>
 		</div>
 	);
 };
