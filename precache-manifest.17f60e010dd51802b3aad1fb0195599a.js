d32d2338d8d2fdde7a3595a17839fe44b0f68596
chato1337 <chato1337@gmail.com>
2020-10-18 15:21:06 -0500

    Updates

---
+++ precache-manifest.17f60e010dd51802b3aad1fb0195599a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a4c12b7e3bc43127d09dff9cf12449",
+    "url": "/frontend-cv/index.html"
+  },
+  {
+    "revision": "400c810e32d5d3213835",
+    "url": "/frontend-cv/static/css/main.9a894f1d.chunk.css"
+  },
+  {
+    "revision": "ccfc704d30b528cd561c",
+    "url": "/frontend-cv/static/js/2.93240176.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/frontend-cv/static/js/2.93240176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400c810e32d5d3213835",
+    "url": "/frontend-cv/static/js/main.106f2ed2.chunk.js"
+  },
+  {
+    "revision": "c337791299e42abe3527",
+    "url": "/frontend-cv/static/js/runtime-main.4658e815.js"
+  }
+]);
