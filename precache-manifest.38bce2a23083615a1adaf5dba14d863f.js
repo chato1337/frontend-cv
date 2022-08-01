c2d3228ff5dc863782b78645b79cedeef34b0efa
chato1337 <chato1337@gmail.com>
2022-08-01 17:34:23 -0500

    Updates

---
+++ precache-manifest.38bce2a23083615a1adaf5dba14d863f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9903863367cbf68f29b0f6d7392d52cd",
+    "url": "/frontend-cv/index.html"
+  },
+  {
+    "revision": "1bb41dbcdacf8c4bda77",
+    "url": "/frontend-cv/static/css/main.88f66caf.chunk.css"
+  },
+  {
+    "revision": "50935abddb8d55692731",
+    "url": "/frontend-cv/static/js/2.8caa58fc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/frontend-cv/static/js/2.8caa58fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb41dbcdacf8c4bda77",
+    "url": "/frontend-cv/static/js/main.35ddbe3e.chunk.js"
+  },
+  {
+    "revision": "97b815ef1c0133d77619",
+    "url": "/frontend-cv/static/js/runtime-main.3d9dce9f.js"
+  },
+  {
+    "revision": "8dbcd32834278ff16f33d792cfedfc58",
+    "url": "/frontend-cv/static/media/profile.8dbcd328.jpeg"
+  }
+]);
