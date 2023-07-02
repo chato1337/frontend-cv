3f663b7af27481e53736504b7483a67461fc70c8
chato1337 <chato1337@gmail.com>
2023-07-01 23:16:01 -0500

    Updates

---
+++ precache-manifest.363b00194c20d8e66b3819554f1d90ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe14b5c47ef13101145e31e0a441143b",
+    "url": "/frontend-cv/index.html"
+  },
+  {
+    "revision": "3d192575c6e3210b069a",
+    "url": "/frontend-cv/static/css/main.3167c341.chunk.css"
+  },
+  {
+    "revision": "ecc42c4eadc35cc52fbc",
+    "url": "/frontend-cv/static/js/2.a84428d9.chunk.js"
+  },
+  {
+    "revision": "31ab0097190a9d99b331909bb0d9d9b1",
+    "url": "/frontend-cv/static/js/2.a84428d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d192575c6e3210b069a",
+    "url": "/frontend-cv/static/js/main.ef072e14.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/frontend-cv/static/js/main.ef072e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b815ef1c0133d77619",
+    "url": "/frontend-cv/static/js/runtime-main.3d9dce9f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-cv/precache-manifest.b8301092b474e45791cf8e1c0db13b13.js"
+  "/frontend-cv/precache-manifest.363b00194c20d8e66b3819554f1d90ca.js"
 );
 
 self.addEventListener('message', (event) => {
