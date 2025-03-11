c19698816a20a1cdc19709cb91ae032553f897d4
chato1337 <chato1337@gmail.com>
2025-03-11 18:42:05 -0500

    Updates

---
+++ precache-manifest.02fbbba0ba169659ae7a71bb05b24e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99635578dc00f008b8998df05287ba07",
+    "url": "/frontend-cv/index.html"
+  },
+  {
+    "revision": "0f316a4736ec39016435",
+    "url": "/frontend-cv/static/css/main.f1593792.chunk.css"
+  },
+  {
+    "revision": "8d04d838d01d606a6df2",
+    "url": "/frontend-cv/static/js/2.530f2709.chunk.js"
+  },
+  {
+    "revision": "0f316a4736ec39016435",
+    "url": "/frontend-cv/static/js/main.55b796ac.chunk.js"
+  },
+  {
+    "revision": "c26926862001566d9628",
+    "url": "/frontend-cv/static/js/runtime~main.e24a85b3.js"
+  },
+  {
+    "revision": "8dbcd32834278ff16f33d792cfedfc58",
+    "url": "/frontend-cv/static/media/profile.8dbcd328.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-cv/precache-manifest.b95776703261225339323213f594f47f.js"
+  "/frontend-cv/precache-manifest.02fbbba0ba169659ae7a71bb05b24e58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend-cv/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
